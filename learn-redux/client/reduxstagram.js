b4a38f7674e7ce96951ea98d64fcea78610739d8
Thomas <tate.thomas5595@gmail.com>
2017-09-27 09:22:41 -0600

    Vid 2 Complete

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,12 @@
 // let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+// css styles
+import css from './styles/style.styl';
+
+
+// Import Components
+import Main from './components/Main';
+
+render(<Main/>, document.getElementById('root'));
